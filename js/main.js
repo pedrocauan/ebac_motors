f7e557cdd3d50b1862f2cb79d42922791e66273a
pedrocauan <mainyasuo92@gmail.com>
2024-03-05 11:51:23 -0300

    Site feito durante a aula

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$("document").ready(function () {
+    $("#carrossel-imagens").slick({ autoplay: true });
+});
+
+$(".menu-burger").click(function (e) {
+    $("nav").slideToggle();
+});
+$("#telefone").mask("(00) 00000-0000");
+$("form").validate({
+    rules: {
+        nome: {
+            required: true,
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        telefone: {
+            required: true
+        },
+        mensagem: {
+            required: true
+        },
+        veiculoDeInteresse: {
+            required: true
+        }
+    },
+    messages: {
+        nome: "Por favor insira o seu nome"
+    },
+    submitHandler: function(form) {
+        console.log(form);
+    },
+    invalidHandler: function(e, validator) {
+        let camposIncorretos = validator.numberOfInvalids();
+        console.log(camposIncorretos);
+        if(camposIncorretos)
+            alert(`existem ${camposIncorretos} campos incorretos`);
+    }
+});
+
+$(".lista-veiculos button").click(function() {
+    const destino = $("#contato");
+    const nomeVeiculo = $(this).parent().find("h3").text();
+
+    $("#veiculo-interesse").val(nomeVeiculo);
+
+    $("html").animate({
+        scrollTop: destino.offset().top
+    },
+        100
+    );
+});
